f50c016f03078a8d0781d51a8eb8e9854766160d
kirannv <kiran.d.n.v@gmail.com>
2020-01-13 23:56:51 +0600

    test cherry on stage

--- bhanu.java
+++ bhanu.java
@@ -2,5 +2,9 @@ hi hello hru
 am fine
 checking the cherry pick command
 checking the cherry pick
+<<<<<<< HEAD
 updated in development
 for checking hit pull comman
+=======
+>>>>>>> ded8cc7... cherry1
+>>>>>>> updating again for cherpick test
